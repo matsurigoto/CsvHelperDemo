49d45ed328a83c0bca6b68eb4c5503a13b303a6f
Duran Hsieh <chhsie@microsoft.com>
2022-02-09 08:43:24 +0800

    init

---
+++ CsvHelperDemo/Employee.cs
@@ -0,0 +1,19 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsvHelperDemo
+{
+    public class Employee
+    {
+        [Index(0)]
+        public int Id { get; set; }
+
+        [Index(1)]
+        public string Name { get; set; }
+
+        [Index(2)]
+        public DateTime Birthday { get; set; }
+    }
+}
---
+++ CsvHelperDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace CsvHelperDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Read Section
+            var readConfiguration = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true
+            };
+
+            using (var reader = new StreamReader("Employee.csv"))
+            using (var csv = new CsvReader(reader, readConfiguration))
+            {
+                var records = csv.GetRecords<Employee>();
+
+                foreach (var employee in records)
+                {
+                    Console.WriteLine(employee.Name + "," + employee.Birthday);
+                }
+            }
+
+
+            //Write Section
+            var writeRecords = new List<Employee>
+            {
+             new Employee { Id = 1, Name = "Ina", Birthday=DateTime.Now },
+             new Employee { Id = 2, Name = "Duran", Birthday=DateTime.Now},
+            };
+
+            var writeConfiguration = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = false
+            };
+
+            using (var writer = new StreamWriter("Result.csv"))
+            using (var csv = new CsvWriter(writer, writeConfiguration))
+            {
+                csv.WriteRecords(writeRecords);
+            }
+        }
+    }
+}
